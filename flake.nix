0183ba1c929a1611ed354ee2459636131eb6f1a2
rdk31 <rdk31@protonmail.com>
2024-01-07 13:21:34 +0100

    update

--- flake.nix
+++ flake.nix
@@ -3,52 +3,56 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    esp32 = pkgs.dockerTools.pullImage {
-      imageName = "espressif/idf-rust";
-      imageDigest = "sha256:4d6ca6c1764225eb07374fb3c0584696bf0e9483abf04d075db27b60bc3e3d49";
-      sha256 = "Y8l8B73V+4neNaL3tk0cHkDYW4bWOgTjIRO2fD4Kacw=";
-      finalImageName = "espressif/idf-rust";
-      finalImageTag = "all_latest";
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      esp32 = pkgs.dockerTools.pullImage {
+        imageName = "espressif/idf-rust";
+        imageDigest = "sha256:903d3ac0ac846a8cc82fc7bb1540b1fdb5ea03b18b46145368b28d17f6e05db2";
+        sha256 = "sha256-LreNOzaDUymrBlYRVQJ7Dny4UuYZspFYtM+YuwGvPCI=";
+        finalImageName = "espressif/idf-rust";
+        finalImageTag = "all_latest";
+      };
+    in
+    {
+      packages.x86_64-linux.esp32 = pkgs.stdenv.mkDerivation {
+        name = "esp32";
+        src = esp32;
+        unpackPhase = ''
+          mkdir -p source
+          tar -C source -xvf $src
+        '';
+        sourceRoot = "source";
+        nativeBuildInputs = [
+          pkgs.autoPatchelfHook
+          pkgs.jq
+        ];
+        buildInputs = [
+          pkgs.xz
+          pkgs.zlib
+          pkgs.libxml2
+          # pkgs.python2
+          pkgs.libudev-zero
+          pkgs.stdenv.cc.cc
+        ];
+        buildPhase = ''
+          jq -r '.[0].Layers | @tsv' < manifest.json > layers
+        '';
+        installPhase = ''
+          mkdir -p $out
+          for i in $(< layers); do
+            tar -C $out -xvf "$i" home/esp/.cargo home/esp/.rustup || true
+          done
+          mv -t $out $out/home/esp/{.cargo,.rustup}
+          rmdir $out/home/esp
+          rmdir $out/home
+          # [ -d $out/.cargo ] && [ -d $out/.rustup ]
+        '';
+      };
     };
-  in {
-    packages.x86_64-linux.esp32 = pkgs.stdenv.mkDerivation {
-      name = "esp32";
-      src = esp32;
-      unpackPhase = ''
-        mkdir -p source
-        tar -C source -xvf $src
-      '';
-      sourceRoot = "source";
-      nativeBuildInputs = [
-        pkgs.autoPatchelfHook
-        pkgs.jq
-      ];
-      buildInputs = [
-        pkgs.xz
-        pkgs.zlib
-        pkgs.libxml2
-        pkgs.python2
-        pkgs.libudev-zero
-        pkgs.stdenv.cc.cc
-      ];
-      buildPhase = ''
-        jq -r '.[0].Layers | @tsv' < manifest.json > layers
-      '';
-      installPhase = ''
-        mkdir -p $out
-        for i in $(< layers); do
-          tar -C $out -xvf "$i" home/esp/.cargo home/esp/.rustup || true
-        done
-        mv -t $out $out/home/esp/{.cargo,.rustup}
-        rmdir $out/home/esp
-        rmdir $out/home
-        # [ -d $out/.cargo ] && [ -d $out/.rustup ]
-      '';
-    };
-  };
 }
